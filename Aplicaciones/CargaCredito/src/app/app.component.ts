b264619a7795c93fc6ebe65154ad636f41fd7fbf
jngraziano <jngraziano@gmail.com>
2018-09-26 22:51:29 -0300

    Carga Credito:               -Iconos y splash screen finalizado (falta animac). 	      -RootPage modificado.

--- Aplicaciones/CargaCredito/src/app/app.component.ts
+++ Aplicaciones/CargaCredito/src/app/app.component.ts
@@ -27,7 +27,7 @@ import { Settings } from '../providers';
   <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = FirstRunPage;
+  rootPage = 'WelcomePage';
 
   @ViewChild(Nav) nav: Nav;
 
