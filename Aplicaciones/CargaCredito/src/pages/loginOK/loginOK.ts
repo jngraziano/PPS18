19ecabbd31f5fc98cff03d854c4dc65e8b0e7b70
jngraziano <jngraziano@gmail.com>
2018-10-04 00:09:17 -0300

    Firestore Avanzado 1

--- Aplicaciones/CargaCredito/src/pages/loginOK/loginOK.ts
+++ Aplicaciones/CargaCredito/src/pages/loginOK/loginOK.ts
@@ -6,7 +6,8 @@ import { IonicPage, NavController, ToastController, ActionSheetController, Modal
 
 import { User } from '../../providers';
 import { MainPage } from '../';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 /**
  * The Welcome Page is a splash page that quickly describes the app,
  * and then directs the user to create an account or log in.
@@ -20,7 +21,8 @@ import { MainPage } from '../';
 })
 export class loginOK {
 
-  ListadoUsuarios: Observable<Usuario[]>;
+  UsuarioColeccionFirebase : AngularFirestoreCollection<Usuario>
+  ListadoUsuariosObser: Observable<Usuario[]>;
   
   loginFields: { email: string, password: string } = {
     email: '',
@@ -37,11 +39,14 @@ export class loginOK {
     public modalCtrl: ModalController,
     public toastCtrl: ToastController,
     public translateService: TranslateService,
+    private fireStoreObj: AngularFirestore,
     public actionSheetCtrl: ActionSheetController,
   ) {
     this.translateService.get('LOGIN_ERROR').subscribe((value) => {
       this.loginErrorString = value;
     })
+
+   
    }
 
  
@@ -53,76 +58,35 @@ export class loginOK {
   //Funcion que va desde el form que borre en el html.
   doLogin() {
 
-   /* funcion original:
-    this.user.login(this.loginFields).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
-      this.navCtrl.push(MainPage);
-      // Unable to log in
-      let toast = this.toastCtrl.create({
-        message: this.loginErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
-  */
+   
 
-    // MIO FALTA FIREBASE
+    // SIN FIREBASE
+    // let flagNotOK: number=0;
+    // let user: Usuario = new Usuario("","");
 
-    let flagNotOK: number=0;
-    let user: Usuario = new Usuario("","");
+    // if (this.loginFields.email != "" && this.loginFields.password != "") {
+    //   user.usMail=this.loginFields.email  
+    //   user.usPass=this.loginFields.password;
+    //   this.navCtrl.push(MainPage);
+    // }
+    // else{
+    //   let toast = this.toastCtrl.create({
+    //           message: "Debe completar los campos.",
+    //           duration: 3000,
+    //           position: 'top'
+    //         });
+    //         toast.present();
+    //         flagNotOK=1;
 
-    if (this.loginFields.email != "" && this.loginFields.password != "") {
-      user.usMail=this.loginFields.email  
-      user.usPass=this.loginFields.password;
-      this.navCtrl.push(MainPage);
-    }
-  //  else if (condition) {
-     
-  //  }
-    else{
-      let toast = this.toastCtrl.create({
-              message: "Debe completar los campos.",
-              duration: 3000,
-              position: 'top'
-            });
-            toast.present();
-            flagNotOK=1;
-
-    }
+    // }
    
       
-    // if(us)
-
-  //   if(this.ListadoUsuarios == undefined)
-  //   {
-  //     let toast = this.toastCtrl.create({
-  //       message: "Debe completar los campos.",
-  //       duration: 3000,
-  //       position: 'top'
-  //     });
-  //     toast.present();
-  //     flagNotOK=1;
-  //   }
-  //   else {
-  //   this.ListadoUsuarios.forEach((element) =>{
-  //     this.cuentas = element;
-  //     let user: Usuario = this.cuentas.find(elem => ( this.loginFields.email == elem.usMail 
-  //                                                && (this.loginFields.password == elem.usPass)));
-  
-  //     if (user !== undefined) {
-  //       this.navCtrl.push(MainPage);
-        
-  //     }
-  //     else {
-  //       alert("Acceso denegado.");
-  //     }
-    
-  //   });
+  this.UsuarioColeccionFirebase = this.fireStoreObj.collection<Usuario>('usuarios', ref=> ref.orderBy('id','asc'));
+  this.ListadoUsuariosObser = this.UsuarioColeccionFirebase.valueChanges();
+  // this.ListadoUsuariosObser.subscribe(x => {
 
   // }
-
+    
 
 
   }
